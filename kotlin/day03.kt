13503ed9a815aadeea9506bc79c3cd61d7751cd6
Björn Westlin <bwestlin@gmail.com>
2019-11-07 12:45:50 +0100

    Day 3 solution in kotlin

---
+++ kotlin/day03.kt
@@ -0,0 +1,25 @@
+import kotlin.system.measureTimeMillis
+
+fun parseSides(l: String) = l.split(' ').filter(String::isNotEmpty).map(String::toInt).toTypedArray()
+
+fun validTriangle(a: Int, b: Int, c: Int) = (a + b > c) && (a + c > b) && (b + c > a)
+
+fun List<Array<Int>>.countValidTriangles() = this.filter{ s -> validTriangle(s[0], s[1], s[2]) }.count()
+
+fun main() {
+    val ms = measureTimeMillis {
+        val input = generateSequence(::readLine).map(::parseSides).toList()
+
+        val part1 = input.countValidTriangles()
+
+        val part2 = input.chunked(3).flatMap { c ->
+            (0..2).map { i ->
+                arrayOf(c[0][i], c[1][i], c[2][i])
+            }
+        }.countValidTriangles()
+
+        println("Part1: ${part1}")
+        println("Part2: ${part2}")
+    }
+    println("It took ${ms}ms")
+}
