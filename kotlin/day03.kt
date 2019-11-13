df836bdbf73026f1e2a9c2adc5159445259496ba
Björn Westlin <bwestlin@gmail.com>
2019-11-13 23:49:36 +0100

    Improve kotlin day3 a bit

--- kotlin/day03.kt
+++ kotlin/day03.kt
@@ -4,7 +4,13 @@ fun parseSides(l: String) = l.split(' ').filter(String::isNotEmpty).map(String::
 
 fun validTriangle(a: Int, b: Int, c: Int) = (a + b > c) && (a + c > b) && (b + c > a)
 
-fun List<Array<Int>>.countValidTriangles() = this.filter{ s -> validTriangle(s[0], s[1], s[2]) }.count()
+fun List<Array<Int>>.countValidTriangles() = this.filter { s -> validTriangle(s[0], s[1], s[2]) }.count()
+
+fun List<Array<Int>>.transposed() = this.chunked(3).flatMap { c ->
+    (0..2).map { i ->
+        arrayOf(c[0][i], c[1][i], c[2][i])
+    }
+}
 
 fun main() {
     val ms = measureTimeMillis {
@@ -12,11 +18,7 @@ fun main() {
 
         val part1 = input.countValidTriangles()
 
-        val part2 = input.chunked(3).flatMap { c ->
-            (0..2).map { i ->
-                arrayOf(c[0][i], c[1][i], c[2][i])
-            }
-        }.countValidTriangles()
+        val part2 = input.transposed().countValidTriangles()
 
         println("Part1: ${part1}")
         println("Part2: ${part2}")
