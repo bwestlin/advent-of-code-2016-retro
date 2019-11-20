0f4456737167b57ef08b4cde472c8148ddb81a32
Björn Westlin <bwestlin@gmail.com>
2019-11-21 00:48:09 +0100

    Increase performance ~9x for rust day 5 solution

--- rust/src/day05.rs
+++ rust/src/day05.rs
@@ -1,25 +1,46 @@
 extern crate utils;
 extern crate md5;
+extern crate rayon;
+extern crate num_cpus;
 
 use std::env;
 use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
+use rayon::prelude::*;
 use utils::*;
 
 type Input = String;
 
-fn solve(input: &Input) -> (String, String) {
+fn password_matches(input: &Input, range: std::ops::Range<usize>) -> Vec<(usize, usize, usize)> {
     const PREFIX: [u8; 3] = [0xFF, 0xFF, 0xF0];
-    const HEX: [char; 16] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let mut matches = vec![];
 
-    let door_id = input.as_str();
-    let mut password1 = String::new();
-    let mut password2: Vec<_> = (0..8).map(|_| '_').collect();
+    let mut seed: Vec<u8> = vec![];
+    seed.extend_from_slice(format!("{}{}", input, range.start).as_bytes());
+    let start = range.start;
 
-    'outer: for index in 0..std::u64::MAX {
-        let idx_s = format!("{}", index);
-        let digest = md5::compute(format!("{}{}", door_id, idx_s));
+    'outer: for index in range {
+
+        // Inrement seed
+        if index > start {
+            let mut remainder = 1;
+            for i in 0..(seed.len() - input.len()) {
+                let s_idx = seed.len() - 1 - i;
+                if seed[s_idx] == '9' as u8 {
+                    seed[s_idx] -= 9;
+                } else {
+                    seed[s_idx] += 1;
+                    remainder = 0;
+                    break;
+                }
+            }
+            if remainder != 0 {
+                seed.insert(input.len(), '1' as u8);
+            }
+        }
+
+        let digest = md5::compute(&seed);
 
         for i in 0..PREFIX.len() {
             if digest[i] & PREFIX[i] != 0 {
@@ -27,16 +48,41 @@ fn solve(input: &Input) -> (String, String) {
             }
         }
 
-        let nibble6 = (digest[2] & 0x0F) as usize;
+        matches.push((index, (digest[2] & 0x0F) as usize, ((digest[3] >> 4) & 0x0F) as usize));
+    }
 
-        if password1.len() < 8 {
-            password1.push(HEX[nibble6]);
-        }
+    matches
+}
 
-        if nibble6 < 8 && password2[nibble6] == '_' {
-            password2[nibble6] = HEX[((digest[3] >> 4) & 0x0F) as usize];
-            if !password2.contains(&'_') {
-                break;
+fn solve2(input: &Input) -> (String, String) {
+    const HEX: [char; 16] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    let n_per_thread = 100000;
+    let n_threads = num_cpus::get();
+
+    let mut password1 = String::new();
+    let mut password2: Vec<_> = (0..8).map(|_| '_').collect();
+
+    'outer: for i in (1..).step_by(n_per_thread * n_threads) {
+        let ranges: Vec<_> = (0..n_threads)
+            .map(|ti| (i + (ti * n_per_thread))..(i + (ti * n_per_thread) + n_per_thread))
+            .collect();
+
+        let mut matches: Vec<_> = ranges.into_par_iter()
+            .flat_map(|r| password_matches(input, r))
+            .collect();
+
+        matches.sort_unstable_by(|(a, _, _), (b, _, _)| a.cmp(b));
+
+        for (_, nibble6, nibble7) in matches {
+            if password1.len() < 8 {
+                password1.push(HEX[nibble6]);
+            }
+
+            if nibble6 < 8 && password2[nibble6] == '_' {
+                password2[nibble6] = HEX[nibble7];
+                if !password2.contains(&'_') {
+                    break 'outer;
+                }
             }
         }
     }
@@ -47,7 +93,7 @@ fn solve(input: &Input) -> (String, String) {
 fn main() {
     measure(|| {
         let input = input().expect("Input failed");
-        let (part1, part2) = solve(&input);
+        let (part1, part2) = solve2(&input);
         println!("Part1: {}", part1);
         println!("Part2: {}", part2);
     });
@@ -75,11 +121,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve(&as_input(INPUT)).0, "18f47a30".to_string());
+        assert_eq!(solve2(&as_input(INPUT)).0, "18f47a30".to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve(&as_input(INPUT)).1, "05ace8e3".to_string());
+        assert_eq!(solve2(&as_input(INPUT)).1, "05ace8e3".to_string());
     }
 }
